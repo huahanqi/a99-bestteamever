5b46096d86c7602390998355f361d182629edc26
Hanqi Hua <huahanqi@live.unc.edu>
2021-12-02 20:54:24 -0500

    editted endpoint but fail

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ var md5 = require("md5")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Set server port
 var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
+
+app.use("/a99-bestteamever", express.static(__dirname));
+
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
     res.json({"message":"Your API works! (200)"});
@@ -32,18 +36,18 @@ app.post("/app/new/", (req, res) => {
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users/all", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo").all();
-	res.status(200).json(stmt);
+	//const stmt = db.prepare("SELECT * FROM userinfo").all();
+	res.status(200).sendFile(__dirname + "/index.html");
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/user/:user/:pass/", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE user = ? AND pass= ?").get(req.params.user, md5(req.params.pass));
+app.get("/app/user/:user/:pass/", (req, res) => {
+	try{
+		const stmt = db.prepare("SELECT * FROM userinfo WHERE user = ? AND pass= ?").get(req.params.user, md5(req.params.pass));
+	}	
+	catch(error) {
+		res.sendFile(__dirname + "/Something_went_wrong.html");
+	}
+	
 	res.status(200).json({"message":`1 record has been found: User ${req.params.user} welcome back!`});
-});
-
-// Default response for any other request
-app.use(function(req, res){
-	res.json({"message":"Something Went Wrong!"});
-    res.status(404);
 });
